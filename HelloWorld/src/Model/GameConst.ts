3d5391f29bda632a7b0f315e7e028d58829e7ffa
xieqirong <klang001she@163.com>
2017-02-17 17:13:34 +0800

    宏定义文件

---
+++ HelloWorld/src/Model/GameConst.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by youmo on 2017/2/17.
+ */
+
+var GameConst = GameConst || {};
+
+GameConst.MaxWidth = egret.MainContext.instance.stage.stageWidth;
+GameConst.MaxHeight = egret.MainContext.instance.stage.stageHeight;
