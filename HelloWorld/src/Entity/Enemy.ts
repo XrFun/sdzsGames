eed3e0560df9ad59e9a30dd2ea1a56ab4bc26b05
XieJingWen <1106153555@qq.com>
2017-02-22 11:01:26 +0800

    enemy animation

--- HelloWorld/src/Entity/Enemy.ts
+++ HelloWorld/src/Entity/Enemy.ts
@@ -6,12 +6,20 @@
  * 敌人类,BOSS系文哥.
  */
 class Entity extends egret.Sprite {
-    public constructor() {
+    public enemyMC : egret.MovieClip;
+    public mcDataFactory :　egret.MovieClipDataFactory;　
+    public constructor(enemyType:number) {
         super();
-        this.init();
+        this.init(enemyType);
     }
 
-    private init():void {
-        console.log("文哥好靓仔");
+    private init(enemyType):void {
+      //加载enemy 行走的动画****************
+      var data = RES.getRes("hero"+ enemyType + "_json");
+      var texture = RES.getRes("hero"+ enemyType + "_png");
+      this.mcDataFactory = new egret.MovieClipDataFactory(data, texture);
+      this.enemyMC = new egret.MovieClip(this.mcDataFactory.generateMovieClipData("walk"));
+      this.addChild(this.enemyMC);
+      this.enemyMC.play(-1);
     }
 }
